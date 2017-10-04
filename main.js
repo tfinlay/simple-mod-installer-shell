981eab2c0f679b9a78afcf9b9cd2dbf88f3debd3
Tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2017-10-04 13:10:37 +1300

    removed ctypes dep from util stopped developer tools from showing

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*
 Sourced from https://www.fyears.org/2015/06/electron-as-gui-of-python-apps.html
 */
-
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
 
 var mainWindow = null;
 
@@ -14,33 +14,101 @@ app.on('window-all-closed', function() {
     //}
 });
 
+console.log("Starting Simple Mod Installer...")
+
+
+
 app.on('ready', function() {
     // call python?
+	// Set up Sudoer to start python as admin
+	/*
+	var Sudoer = require('electron-sudo').default;
+	
+	var options = {name: 'Simple Mod Installer'};
+	var sudoer = new Sudoer(options);
+	
+	sudoer.spawn('nano', ['hi there']).then(function(cp) {app.quit()});
+	
+	sudoer.spawn(
+		path.join('bin', 'python'),
+		[path.join('bin', 'simple_mod_installer', 'simple_mod_installer', '__init__.py')] // Change this to match Python's startup file location.
+	).then(function(cp) {
+		cp.stdout.setEncoding('utf8')
+		cp.stderr.setEncoding('utf8')
+		
+		cp.stdout.on('data', (data) => {
+			console.log(data);
+		});
+		
+		cp.stderr.on('data', (data) => {
+			console.error(data);
+		});
+		
+		cp.on('close', (code) => {
+			console.log("Server closed with code: " + code);
+			
+			if (code != "0") {
+				var btnIndex = electron.dialog.showMessageBox({
+					type: "warning",
+					buttons: ["Take me to the log files", "close"],
+					title: "Simple Mod Installer: is everything alright?",
+					message: "The server has exited with a non-zero condition.\nThis may be a sign that something has gone wrong with the Simple Mod Installer. If something has gone wrong, please send a report which includes either the output of the console, or the data in the log files.\n\nIf everything's fine then you can safely ignore this message."
+				});
+				
+				if (btnIndex == 0) {
+					// Show the log files
+					electron.shell.showItemInFolder(require('path').resolve(__dirname) + '/simple_mod_installer/simple_mod_installer/simple.log')
+				}
+				
+				console.warn("Server closed with a bad code, something may have gone wrong. If something has gone wrong, please send a report which includes either the output of the console here, or the output in the log files.")
+			}
+			
+			app.quit()
+		});
+	});*/
+	
     var subpy = require('child_process').spawn(
-		'python3',
-		['./simple_mod_installer/__init__.py'] // Change this to match Python's startup file location.
+		path.join('bin', 'python'),
+		[path.join('bin', 'simple_mod_installer', '__init__.py')] // Change this to match Python's startup file location.
 		/*{
 		    0,
 		    'pipe'
 		}*/
 	);
-    
-    
-    subpy.stdout.setEncoding('utf8')
-    subpy.stderr.setEncoding('utf8')
-    
-    subpy.stdout.on('data', (data) => {
-        console.log(data);
-    });
-    
-    subpy.stderr.on('data', (data) => {
-        console.error(data);
-    });
-    
-    subpy.on('close', (code) => {
-        console.log("Server closed with code: " + code);
-    });
-    
+	
+	subpy.stdout.setEncoding('utf8')
+	subpy.stderr.setEncoding('utf8')
+	
+	subpy.stdout.on('data', (data) => {
+		console.log(data);
+	});
+	
+	subpy.stderr.on('data', (data) => {
+		console.error(data);
+	});
+	
+	subpy.on('close', (code) => {
+		console.log("Server closed with code: " + code);
+		
+		if (code != "0") {
+			var btnIndex = electron.dialog.showMessageBox({
+				type: "warning",
+				buttons: ["Take me to the log files", "close"],
+				title: "Simple Mod Installer: is everything alright?",
+				message: "The server has exited with a non-zero condition.\nThis may be a sign that something has gone wrong with the Simple Mod Installer. If something has gone wrong, please send a report which includes either the output of the console, or the data in the log files.\n\nIf everything's fine then you can safely ignore this message."
+			});
+			
+			if (btnIndex == 0) {
+				// Show the log files
+				electron.shell.showItemInFolder(require('path').resolve(__dirname) + '/simple_mod_installer/simple_mod_installer/simple.log')
+			}
+			
+			console.warn("Server closed with a bad code, something may have gone wrong. If something has gone wrong, please send a report which includes either the output of the console here, or the output in the log files.")
+		}
+		
+		app.quit()
+	});
+	
     //var subpy = require('child_process').spawn('./dist/hello.exe');
     var rq = require('request-promise');
     var mainAddr = 'http://localhost:5000';
@@ -54,7 +122,7 @@ app.on('ready', function() {
         });
 
         mainWindow.loadURL('http://localhost:5000');
-        mainWindow.webContents.openDevTools();
+        //mainWindow.webContents.openDevTools();
         mainWindow.on('closed', function() {
           mainWindow = null;
           subpy.kill('SIGINT');
